855a1712f5cd104809ff1f8aa3159a5e47e1fd37
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-03 09:55:03 -0300

    Exercicio 1 finalizado

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import Ex1.Produto;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        String nome;
+        double valor;
+        int quantidade;
+        double porcentagem;
+
+        System.out.print("Digite o nome do produto: ");
+        nome = inp.next();
+        System.out.print("Digite o valor do produto: R$");
+        valor = inp.nextDouble();
+        System.out.print("Digite o quantidade do produto: ");
+        quantidade = inp.nextInt();
+
+        Produto pro = new Produto(nome, valor, quantidade);
+
+        System.out.println("Porcentagem do aumento: ");
+        porcentagem = inp.nextDouble();
+        pro.aumentarValor(porcentagem);
+
+    }
+}
